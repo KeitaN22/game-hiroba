dd5ce1c60a556bdffd142885d371d11fd6914e14
keita <keita.usuzumi@gmail.com>
2026-08-10 11:59:30 +0900

    アプリづくりゲームの ボタンを おすと がめんが きえて、あおいボタンで また あそべるようにした

--- games.js
+++ games.js
@@ -345,7 +345,7 @@ const GAMES = [
     title: 'そうすけの アプリづくり',
     emoji: '📱',
     color: '#7EC8E3',
-    path: 'games/app-maker/index.html?v=3',
+    path: 'games/app-maker/index.html?v=4',
     desc: 'ボタンや テキスト、グラフなど いろんな どうぐを つかって、じぶんだけの アプリを つくろう！',
   },
 ];
