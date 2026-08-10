778b2f61f6438d28da43937cdc9a2984c4ce6be7
keita <keita.usuzumi@gmail.com>
2026-08-10 11:51:04 +0900

    アプリづくりゲームの どうぐを もっとあそべるようにした
    がぞう・ひょうか・つうち・おんがく・けんさく・はいけいを タップして あそべるようにしました

--- games.js
+++ games.js
@@ -345,7 +345,7 @@ const GAMES = [
     title: 'そうすけの アプリづくり',
     emoji: '📱',
     color: '#7EC8E3',
-    path: 'games/app-maker/index.html?v=2',
+    path: 'games/app-maker/index.html?v=3',
     desc: 'ボタンや テキスト、グラフなど いろんな どうぐを つかって、じぶんだけの アプリを つくろう！',
   },
 ];
