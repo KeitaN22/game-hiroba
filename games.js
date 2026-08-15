bdae91ce4bc829f85f95854a4da3af3d8e68cacf
keita <keita.usuzumi@gmail.com>
2026-08-15 11:26:34 +0900

    おうちで なにするゲームの マップを もっと ひろく した
    アリーナの たかさと はばを おおきくして、かべの すきまも ふとくしました

--- games.js
+++ games.js
@@ -401,7 +401,7 @@ const GAMES = [
     title: 'そうすけの おうちで なにする？',
     emoji: '🏠',
     color: '#FFD9A0',
-    path: 'games/home-life/index.html?v=2',
+    path: 'games/home-life/index.html?v=3',
     desc: 'やじるしで おうちの マップを うごいて、キッチンや おふろなど 9つの おへやで いろんな ことを やってみよう！',
   },
 ];
